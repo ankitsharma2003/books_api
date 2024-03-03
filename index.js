8a371bc1c22fa049d22fc433ae334e53b5a5c462
Ankit <ankitsharmaaa2003@gmail.com>
2024-03-03 09:56:32 +0530

    add frontend url

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(morgan("common"));
 app.use(cookieParser());
 app.use(cors({
     credentials : true,
-    origin : 'http://localhost:3000'
+    origin : 'https://books-frontend-nu.vercel.app'
 }));
 
 //router
